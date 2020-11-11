a1e3ad787059743cc113a5aee010386ebc7578f8
tuntuntuntuntun <tuntuntun02b@gmail.com>
2020-11-11 21:06:25 +0900

    ヘッダーを作成

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li>WORKS</li>
+                <li>FEATURE</li>
+                <li>ABOUT</li>
+                <li>SKILL</li>
+                <li>CONTACT</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+    nav {
+        margin: 0;
+        padding: 0;
+        border-bottom: 1px solid #000;
+    }
+
+    ul {
+        margin: 0;
+        display: flex;
+        justify-content: center;
+        list-style: none;
+    }
+
+    li {
+        margin: 10px;
+    }
+</style>
